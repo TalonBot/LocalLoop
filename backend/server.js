5d5d9f62a42022323d4e410d419ff74dd5022602
Georgi Dimov <gjorgji.dimov@student.um.si>
2025-06-04 01:53:25 +0200

    cors fix

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,7 @@ app.use("/webhook", require("./routes/webhook"));
 app.use(express.json());
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://localloop.onrender.com",
     credentials: true,
   })
 );
